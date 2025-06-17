58ac5cc953f3ad907ca3b50930317d4aeeaff510
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-06-17 09:19:38 +0900

    Format python

--- format.sh
+++ format.sh
@@ -1,1 +1,4 @@
 find . -type f \( -name "*.cpp" -o -name "*.hpp" \) | xargs clang-format -i -style=Chromium
+find . -maxdepth 1 -name "*py"|xargs python3 -m autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports
+python3 -m black .  --line-length 100
+python3 -m isort .
